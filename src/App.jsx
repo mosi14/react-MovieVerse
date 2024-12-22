f96a380eea67e1aa2f8d2f1d23ee5c51b6af9868
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-22 23:00:06 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-5xl text-red-500'>
+      <h1  className='font-nsans-light'>Netflix</h1>
+      <h1>Netflix</h1>
+      <h1  className='font-nsans-medium'>Netflix</h1>
+      <h1 className='font-nsans-bold'>Netflix</h1>
+      
+    </div>
+  )
+}
