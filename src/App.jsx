2094a89e755af40ce921a2e7f053071c20e693ec
mosio14 <mostafa.asadollahy@gmail.com>
2024-12-24 11:09:24 +0100

    create some of our pages and navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className='text-5xl text-red-500'>
-      <h1  className='font-nsans-light'>Netflix</h1>
-      <h1>Netflix</h1>
-      <h1  className='font-nsans-medium'>Netflix</h1>
-      <h1 className='font-nsans-bold'>Netflix</h1>
-      
-    </div>
-  )
+    <>
+    <Navbar />  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="absolute w-full flex justify-between items-center p-5 z-50">
+      <Link to="/">
+        <h1 className="uppercase text-red-600 font-nsans-Bold cursor-pointer text-5xl">
+          Netflix
+        </h1>
+      </Link>
+      <div>
+        <Link to="/login">
+          <button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer capitalize">
+            Login
+          </button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer capitalize">
+            sign up
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
