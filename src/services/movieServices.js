56464064efb948502139ce01d1b899c7978fd436
mosio14 <mostafa.asadollahy@gmail.com>
2025-01-01 17:48:22 +0100

    Add new api link

--- src/services/movieServices.js
+++ src/services/movieServices.js
@@ -10,9 +10,24 @@ const endpoints = {
   search: `${baseURL}/search/movie?api_key=${key}&query=`,
   comedy: `${baseURL}/discover/movie?api_key=${key}&with_genres=35&query=comedy&language=en-US&page=1&include_adult=false`,
   trending: `${baseURL}/movie/popular?api_key=${key}&language=en-US&page=2`,
+  //details: `${baseURL}/movie/${movie_id}?api_key=${key}&language=en-US`,
+  //videos:'https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${key}'
+  //watchyoutube: `https://www.youtube.com/watch?v={key}`
 };
 
-export function createImageUrl(filename, size){
+export function fetchMovieDetails(movie_id) {
+  return `${baseURL}/movie/${movie_id}?api_key=${key}&language=en-US`;
+}
+export function fetchMovieCasts(movie_id) 
+{
+  return `${baseURL}/movie/${movie_id}/credits?api_key=${key}&language=en-US`
+}
+
+export function fetchMovieVideos(movie_id) {
+  return `${baseURL}/movie/${movie_id}/videos?api_key=${key}&language=en-US`;
+}
+
+export function createImageUrl(filename, size) {
   return `https://image.tmdb.org/t/p/${size}/${filename}`;
 }
 
