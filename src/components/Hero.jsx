89154248a32750a0e937c4441b633fdb7dc0f74b
mosio14 <mostafa.asadollahy@gmail.com>
2025-01-01 17:57:38 +0100

    add link to play button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import endpoints,{createImageUrl} from "../services/movieServices";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import endpoints, { createImageUrl } from "../services/movieServices";
 
 function Hero() {
   const [movie, setMovie] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -25,24 +27,34 @@ function Hero() {
     return str.length > length ? str.slice(0, length) + "..." : str;
   };
 
-  const { title, backdrop_path, release_date, overview } = movie;
+  const handlePlayMovie = (movieId) => {
+    navigate(`/movie/${movieId}`);
+  };
+
+  const { id, title, backdrop_path, release_date, overview } = movie;
   return (
     <>
       <div className="w-full h-[550px] lg:h-[850px]">
         <div className="w-full h-full">
           <div className="absolute w-full h-[550px] lg:h-[850px] bg-gradient-to-r from-black" />
           <img
-            src={createImageUrl(backdrop_path,'original')}
+            src={createImageUrl(backdrop_path, "original")}
             alt={title}
             className="w-full h-full object-cover object-top"
           />
           <div className="absolute w-full top-[40%] lg:top-[50%] p-4 md:p-8">
             <h1 className="text-3xl md:text-6xl font-nsans-Bold">{title}</h1>
             <div className="mt-8 mb-4">
-              <button className="capitalize border bg-gray-300 text-black py-2  px-5">
+              <button
+                className="capitalize border bg-gray-300 text-black py-2 px-5"
+                onClick={() => handlePlayMovie(id)}
+              >
                 play
               </button>
-              <button className="apitalize border border-gray-300 py-2 px-5 ml-4">
+              <button
+                className="apitalize border border-gray-300 py-2 px-5 ml-4"
+                onClick={() => handlePlayMovie(id)}
+              >
                 watch later
               </button>
             </div>
